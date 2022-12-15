265c807566797d5f5c85fd26365ed28125231280
TKTheTechie <thomas.kunnumpurath@solace.com>
2022-12-15 11:23:24 -0600

    Update index.js

--- index.js
+++ index.js
@@ -4,25 +4,29 @@ const DAPR_HOST = process.env.DAPR_HOST || "http://localhost";
 const DAPR_HTTP_PORT = process.env.DAPR_HTTP_PORT || "3501";
 const SERVER_HOST = process.env.SERVER_HOST || "127.0.0.1";
 const SERVER_PORT = process.env.APP_PORT || 5002;
-const PUBSUB_NAME = "solace"
+const PUBSUB_NAME = "solace";
 
 async function main() {
   const server = new DaprServer(SERVER_HOST, SERVER_PORT, DAPR_HOST, DAPR_HTTP_PORT);
-    
+
 
   // Dapr subscription routes orders topic to this route
-  server.pubsub.subscribe(PUBSUB_NAME, "queue://orders-queue", (data) => console.log("Subscriber received: " + JSON.stringify(data)));
- 
+  server.pubsub.subscribe(PUBSUB_NAME, "topic:retail/orders", (data) => console.log("Subscriber received: " + data));
+  // Dapr subscription routes orders queue to this route
+  // server.pubsub.subscribe(PUBSUB_NAME, "queue:retail/orders", (data) => console.log("Subscriber received: " + data));
+
+
+
   await server.start();
   await publish();
 }
 
-async function publish(){
+async function publish() {
   const client = new DaprClient(DAPR_HOST, DAPR_HTTP_PORT, CommunicationProtocolEnum.HTTP);
 
-  for(var i=0;i<5;i++){
-    client.pubsub.publish(PUBSUB_NAME, "retail/orders",'{"orderId": "'+i+'"}')
- }
+  for (var i = 0; i < 5; i++) {
+    client.pubsub.publish(PUBSUB_NAME, "topic:retail/orders", '{"orderId": "' + i + '"}');
+  }
 
 }
 
